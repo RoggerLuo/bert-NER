bdd5b159e3979ce7d34f22963532c6e40dc77ed5
Roger <luojie.5408@163.com>
2019-06-03 10:19:10 +0800

    Update

---
+++ baidu_parsetree.js
@@ -0,0 +1,33 @@
+var AipNlpClient = require("baidu-aip-sdk").nlp;
+
+
+var APP_ID = '15737243'
+var API_KEY = 'MmGcFmcpYeG6vEKEBc20K9zP'
+var SECRET_KEY = 'fGLes8qGvGrntH1Cx75hnSXoGK5dgmcN'
+
+
+// 新建一个对象，建议只保存一个对象调用服务接口
+var client = new AipNlpClient(APP_ID, API_KEY, SECRET_KEY);
+
+
+var text = "张飞";
+
+// 调用依存句法分析
+client.depparser(text).then(function(result) {
+    console.log(JSON.stringify(result));
+}).catch(function(err) {
+    // 如果发生网络错误
+    console.log(err);
+});
+
+// 如果有可选参数
+var options = {};
+options["mode"] = "1";
+
+// 带参数调用依存句法分析
+client.depparser(text, options).then(function(result) {
+    console.log(JSON.stringify(result));
+}).catch(function(err) {
+    // 如果发生网络错误
+    console.log(err);
+});;
